7a3e38ad953140e4f6862cbc0e03da5953a0fa0e
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2024-04-02 21:32:44 +0200

    fix: issues with too many open connections in nextjs

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 // Enable logging
 // https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/logging
-const prisma = new PrismaClient();
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  //@ts-ignore
+  if (!global.prisma) {
+    //@ts-ignore
+    global.prisma = new PrismaClient();
+  }
+  //@ts-ignore
+  prisma = global.prisma;
+}
+
 export default prisma;
