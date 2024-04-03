f868df064e1051ad460f7cc415c3c57923bd551c
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2024-04-03 17:15:10 +0200

    refactor: no need for liked prompts

--- src/prisma/queries.ts
+++ src/prisma/queries.ts
@@ -91,53 +91,6 @@ export const getPrompt = async (
   return prompts;
 };
 
-export const getLikedPrompts = async (
-  results = 20,
-  page = 0,
-  userId: string,
-): Promise<EnrichedPrompt[]> => {
-  if (results > 100) {
-    throw new Error("Results must be less than or equal 100");
-  }
-  const query = Prisma.sql`SELECT 
-  p.id,
-  p."createdAt",
-  p.title,
-  p.description,
-  p.prompt,
-  p.tags,
-  voteCounts."upVotes",
-  voteCounts."downVotes",
-  voteCounts.reports,
-  u.username
-  FROM 
-  "Post" p
-  LEFT JOIN (
-  SELECT 
-      v."postId",
-      COUNT(*) FILTER (WHERE v."value" = 'UPVOTE') AS "upVotes",
-      COUNT(*) FILTER (WHERE v."value" = 'DOWNVOTE') AS "downVotes",
-      COUNT(*) FILTER (WHERE v."value" = 'REPORT') AS reports
-  FROM 
-      "Vote" v   
-  GROUP BY 
-      v."postId"
-  ) AS voteCounts ON voteCounts."postId" = p.id
-  LEFT JOIN "User" u ON u.id = p."authorId"
-  where p.id in (select "postId" from "Like" l where l."userId"=${userId})
-  ORDER BY 
-  voteCounts."upVotes" DESC, 
-  p."createdAt" DESC
-  LIMIT ${results} OFFSET ${page} * ${results}
-  `;
-  const prompts = (await prisma.$queryRaw(
-    query,
-    results,
-    page,
-  )) as EnrichedPrompt[];
-  return prompts;
-};
-
 export const searchPrompts = async (
   results = 20,
   page = 0,
