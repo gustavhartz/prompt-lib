8706e9bc1d53c3e666e9280f154d1da6a1a6cf1e
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2024-04-03 22:47:39 +0200

    feat: searching with query params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import PageLayout from "../components/PageLayout"; // Adjust the path as necessary
 import { cache } from "react";
-import { getPrompts } from "@/prisma/queries";
-import { getSession } from "@auth0/nextjs-auth0";
+import { getPrompts, searchPrompts } from "@/prisma/queries";
 import HeroSearch from "@/components/HeroSearch";
 import PromptList from "@/components/PromptList";
+import { logger } from "@/utils/logger";
 
 interface HomeProps {
-  prompts: ""; // Assuming your Post model corresponds to your prompts
+  searchParams: { query?: string; page?: string; results?: string };
 }
-export const getPromptsSSR = cache(async () => {
-  //TODO: relevant to the user with their likes etc.
-  const prompts = await getPrompts(20, 0);
-  console.log(prompts);
-  return prompts;
-});
+export const getPromptsSSR = cache(
+  async (query?: string, results?: number, page?: number) => {
+    if (query) {
+      return await searchPrompts(results, page, query);
+    }
+    return await getPrompts(results, page);
+  },
+);
 
-export default async function Home({ prompts }: HomeProps) {
-  const topPrompts = await getPromptsSSR();
+export default async function Home(props: HomeProps) {
+  const { query, results, page } = props.searchParams;
+  // ensure results and page are numbers or default to 20 and 0
+  let parsedResults = 20;
+  let parsedPage = 0;
+  try {
+    parsedResults = parseInt(results || "20");
+    parsedPage = parseInt(page || "0");
+  } catch (e) {
+    logger.error(e);
+  }
+  const topPrompts = await getPromptsSSR(query, parsedResults, parsedPage);
+
   return (
     <PageLayout>
       <div className="container mx-auto my-8 flex flex-col min-h-screen">
