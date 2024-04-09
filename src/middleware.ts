2cc34f998b58d759482a7aac19f9145802f1ed94
Gustav Hartz <49193816+gustavhartz@users.noreply.github.com>
2024-04-09 22:52:29 +0200

    feat: some simple ratelimit logic

---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  //analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+// Define which routes you want to rate limit
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|/blocked).*)",
+  ],
+};
+
+export default async function middleware(request: NextRequest) {
+  // disable rate limiting for development
+  if (
+    process.env.NODE_ENV !== "development" &&
+    process.env.UPSTASH_REDIS_REST_URL &&
+    process.env.UPSTASH_REDIS_REST_TOKEN
+  ) {
+    console.log("Rate limiting is disabled in development mode");
+    const ip = request.ip ?? "127.0.0.1";
+    const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+      ip,
+    );
+    const res = success
+      ? NextResponse.next()
+      : NextResponse.redirect(new URL("/blocked", request.url));
+
+    res.headers.set("X-RateLimit-Limit", limit.toString());
+    res.headers.set("X-RateLimit-Remaining", remaining.toString());
+    res.headers.set("X-RateLimit-Reset", reset.toString());
+    return res;
+  }
+  return NextResponse.next();
+}
